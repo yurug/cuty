95e55c619b8e58066aef817604c94408c4757ab1
Yann Régis-Gianas <yrg@pps.univ-paris-diderot.fr>
2015-10-21 09:00:03 +0200

    Skeleton.

--- cutyBackEnd.ml
+++ cutyBackEnd.ml
@@ -12,7 +12,7 @@ let get_element_dimension (e : Dom_html.element Js.t) =
 let new_node id d (width, height) : unit =
   let command =
     Printf.sprintf "new_node (%d, '%s', '%d', '%d');"
-      id (div_as_string d) width height
+      id (div_as_string d) (1 + width) (1 + height)
   in
   Firebug.console##log (command);
   Js.Unsafe.eval_string command
@@ -22,6 +22,7 @@ let new_text_node id text size borderColor =
   s##innerHTML <- Js.string text;
   s##style##fontSize <- Js.string size;
   s##style##padding <- Js.string "1px";
+  s##style##margin <- Js.string "0px";
   s##style##borderColor <- Js.string borderColor;
   s##style##borderWidth <- Js.string "1px";
   s##style##borderStyle <- Js.string "solid";
